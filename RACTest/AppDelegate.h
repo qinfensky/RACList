c54e12498dffefcb45757a98d8833ba15e60efc2
qinfensky <qinfensky@gmail.com>
2015-12-13 22:42:01 +0800

    Initial Commit

---
+++ RACTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RACTest
+//
+//  Created by qinfensky on 15/12/13.
+//  Copyright © 2015年 qinfensky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RACTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RACTest
+//
+//  Created by qinfensky on 15/12/13.
+//  Copyright © 2015年 qinfensky. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
